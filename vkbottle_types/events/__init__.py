bfe40c3f9f5f7778412bea29df546a02e45195cc
50bytes.dev <50bytes.dev@gmail.com>
2025-06-07 18:23:08 +0400

    fix

--- vkbottle_types/events/__init__.py
+++ vkbottle_types/events/__init__.py
@@ -3,8 +3,9 @@ from typing import Union
 from .bot_typings import GroupTypes
 from .enums import GroupEventType, UserEventType
 from .user_typings import UserTypes
+from .bot_events import MessageEvent
 
 Event = Union[UserEventType, GroupEventType]
 
 
-__all__ = ("GroupTypes", "GroupEventType", "UserEventType", "UserTypes", "Event")
+__all__ = ("GroupTypes", "GroupEventType", "UserEventType", "UserTypes", "Event", "MessageEvent")
